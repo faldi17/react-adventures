d32e098c73512b2f772fc7eb0e7d4b15a29e5690
Faldiansyah <faldiansyahk@gmail.com>
2025-02-27 22:55:22 +0700

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./Header"
+import MenuResto from "./MenuResto"
+
 function App() {
-  const name = "Girana"
-  
-  return (
-    <h1>Halo {name} dari ReactJS</h1>
-  )
+    return (
+        <>
+            <Header />
+            <MenuResto />
+        </>
+    )
 }
 
 export default App
---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <>
+            <h1>Menu Resto Muhajir Code 🥄</h1>
+        </>
+    )
+}
+
+export default Header
