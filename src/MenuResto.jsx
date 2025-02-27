166702d523a8e0eaea6f98c2057aa45fbfcc62e6
Faldiansyah <faldiansyahk@gmail.com>
2025-02-27 23:08:44 +0700

    style component

--- src/MenuResto.jsx
+++ src/MenuResto.jsx
@@ -1,13 +1,19 @@
+// import './MenuResto.css'
+
 function MenuResto() {
+    const styleMenu = {
+        backgroundColor: "#333",
+        color: "#fff",
+        padding: "10px",
+        borderRadius: "10px",
+    }
+
     return (
-        <>
-            <div>
-                <b>Nama Menu: </b> <em>Nasi Bakar 🍘</em>
-            </div>
-            <div>
-                <b>Harga: </b> <em>Rp. 20.000</em>
-            </div>
-        </>
+        // <div className="menu-item">
+        <div style={styleMenu}>
+            <div><b>Nama Menu: </b> <em>Nasi Bakar 🍘</em></div>
+            <div><b>Harga: </b> <em>Rp. 20.000</em></div>
+        </div>
     )
 }
 
