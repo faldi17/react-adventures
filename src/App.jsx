42cd63a93ec396963f60d308227e7364a005e4d7
Faldiansyah <faldiansyahk@gmail.com>
2025-03-02 08:25:42 +0700

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Button2 from "./Button2"
+import { useMemo, useState } from "react"
 
 function App() {
+    const [likeCounter, setLikeCounter] = useState(0)
+    const [subscribeCounter, setSubscribeCounter] = useState(0)
+
+    function pesanLike() {
+        console.log(`pesan like rendered`)
+        return (likeCounter < 10) ? 'Like kurang dari 10' : 'Mantap like lebih dari 10'
+    }
+
+    const displayPesanLike = useMemo(() => pesanLike(), [likeCounter])
+
     return (
         <>
-            <Button2 />
+            <p>
+                <button onClick={() => setLikeCounter(likeCounter + 1)}>{likeCounter} Like 👍</button> {displayPesanLike}
+            </p>
+            <p>
+                <button onClick={() => setSubscribeCounter(subscribeCounter + 1)}>{subscribeCounter} Subcribe ▶️</button>
+            </p>
         </>
     )
 }
